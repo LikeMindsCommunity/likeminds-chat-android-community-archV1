6e5ebbc2451b0d73db234b720959071636f5f1cd
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2025-01-08 15:49:06 +0530

    resolved merge comments

--- chatmm/src/main/java/com/likeminds/chatmm/chatroom/detail/view/ChatroomDetailFragment.kt
+++ chatmm/src/main/java/com/likeminds/chatmm/chatroom/detail/view/ChatroomDetailFragment.kt
@@ -5030,9 +5030,6 @@ class ChatroomDetailFragment :
      */
     private fun getIndexOfDate(date: String): Int {
         chatroomDetailAdapter.items().forEachIndexed { index, item ->
-            if (item is ConversationViewData && item.date == date) {
-                return index
-            }
             if (item is ChatroomDateViewData && item.date == date) {
                 return index
             }
